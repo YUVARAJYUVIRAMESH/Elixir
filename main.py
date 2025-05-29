0fcfb3e98718e343c506f95de207492f3c6d1c6c
Vishweswaran <vishweswarann.24cse@kongu.edu>
2025-05-29 02:18:13 +0000

    completed login and register backend

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask
+from flask_login import LoginManager
 from models import db
-from routes import routes
-
+from routes import callRoutes
 
 
 
@@ -10,13 +10,21 @@ def createApp():
 
     app = Flask(__name__)
 
+    login_manager = LoginManager()
+
+    login_manager.init_app(app)
+
+
+
     app.secret_key = 'vanakam'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    
     db.init_app(app)
+
+    routes = callRoutes(app, login_manager)
     app.register_blueprint(routes)
+    
 
 
 
--- routes.py
+++ routes.py
@@ -3,181 +3,182 @@ from flask import render_template, request, redirect, url_for, flash
 from datetime import datetime
 from werkzeug.utils import secure_filename
 import os
-
-# from main import app
 from functions import imagePathCoder
 from models import Books, Users, BooksRented, db
 
 
-# from flask_login import LoginManager, login_user, login_required, logout_user
+from sqlalchemy import or_
+from flask_login import login_user, login_required, logout_user
 
-routes = Blueprint("routes", __name__, template_folder = "templates")
+def callRoutes(app, login_manager):
 
-
-
-# login_manager = LoginManager()
-# login_manager.init_app(app)
-# login_manager.login_view("routes.login")
-#
-#
-#
-# @login_manager.user_loader
-# def load_user(user_id):
-#     return Users.query.get(int(user_id))
+    routes = Blueprint("routes", __name__, template_folder = "templates")
 
 
-@routes.route('/')
-def home():
 
-    print(url_for('routes.admin'))
-    return render_template("home.html")
+    login_manager.login_view = "routes.login"
 
 
-@routes.route("/adminn")
-def admin():
-    dab = request.args.get("db")
-    if dab:
+    @login_manager.user_loader
+    def load_user(user_id):
+        return Users.query.get(int(user_id))
+
+
+    @routes.route('/')
+    def home():
+
+        print(url_for('routes.admin'))
+        return render_template("home.html")
+
+
+    @routes.route("/adminn")
+    def admin():
+        dab = request.args.get("db")
+        if dab:
+            if (dab == "Books"):
+                data = Books.query.all()
+            elif (dab == "Users"):
+                data = Users.query.all()
+            elif (dab == "BooksRented"):
+                data = BooksRented.query.all()
+
+            return render_template(f"admin{dab}.html", data=data, db=dab)
+
+        return render_template("admin.html")
+
+
+    @routes.route("/insert", methods=["POST"])
+    def insert():
+        dab = request.args.get("dab")
+        print(dab)
+
         if (dab == "Books"):
-            data = Books.query.all()
+
+            name = request.form["name"]
+            isbn = request.form["isbn"]
+            genre = request.form["genre"]
+            image = request.files["image"]
+            description = request.form["description"]
+
+            image_path = imagePathCoder(image)
+
+            image.save(image_path)
+            new_book = Books(name, isbn, genre, image_path, description)
+            db.session.add(new_book)
+            db.session.commit()
+
+
         elif (dab == "Users"):
-            data = Users.query.all()
-        elif (dab == "BooksRented"):
-            data = BooksRented.query.all()
+            name = request.form.get("name")
+            email = request.form.get("email")
+            password = request.form.get("password")
 
-        return render_template(f"admin{dab}.html", data=data, db=dab)
+            new_user = Users(name, email, password)
 
-    return render_template("admin.html")
+            db.session.add(new_user)
+            db.session.commit()
 
-
-@routes.route("/insert", methods=["POST"])
-def insert():
-    dab = request.args.get("dab")
-    print(dab)
-
-    if (dab == "Books"):
-
-        name = request.form["name"]
-        isbn = request.form["isbn"]
-        genre = request.form["genre"]
-        image = request.files["image"]
-        description = request.form["description"]
-
-        image_path = imagePathCoder(image)
-
-        image.save(image_path)
-        new_book = Books(name, isbn, genre, image_path, description)
-        db.session.add(new_book)
-        db.session.commit()
+        return redirect(url_for('routes.admin', db=dab))
 
 
-    elif (dab == "Users"):
-        name = request.form.get("name")
-        email = request.form.get("email")
-        password = request.form.get("password")
+    @routes.route("/edit", methods = ["POST"])
+    def edit():
+        id = request.args.get("id")
+        dab = request.args.get("dab")
 
-        new_user = Users(name, email, password)
+        image = request.files.get("image")
 
-        db.session.add(new_user)
-        db.session.commit()
+        if (image):
+            image_Path = imagePathCoder(image)
+            image.save(image_Path)
 
-    return redirect(url_for('routes.admin', db=dab))
+        if (dab == "Books"):
+            edit_book = Books.query.get(id)
 
+            edit_book.name = request.form["name"]
+            edit_book.isbn = request.form["isbn"]
+            edit_book.genre = request.form["genre"]
+            edit_book.description = request.form["description"]
 
-@routes.route("/edit", methods = ["POST"])
-def edit():
-    id = request.args.get("id")
-    dab = request.args.get("dab")
+            if (image and os.path.exists(edit_book.image_url)):
+                os.remove(edit_book.image_url)
+                edit_book.image_url = image_Path
 
-    image = request.files.get("image")
-
-    if (image):
-        image_Path = imagePathCoder(image)
-        image.save(image_Path)
-
-    if (dab == "Books"):
-        edit_book = Books.query.get(id)
-
-        edit_book.name = request.form["name"]
-        edit_book.isbn = request.form["isbn"]
-        edit_book.genre = request.form["genre"]
-        edit_book.description = request.form["description"]
-
-        if (image and os.path.exists(edit_book.image_url)):
-            os.remove(edit_book.image_url)
-            edit_book.image_url = image_Path
-
-        db.session.add(edit_book)
-        db.session.commit()
+            db.session.add(edit_book)
+            db.session.commit()
 
 
-    elif (dab == "Users"):
-        edit_user = Users.query.get(id)
+        elif (dab == "Users"):
+            edit_user = Users.query.get(id)
 
-        edit_user.username = request.form.get("name")
-        edit_user.email = request.form.get("email")
-        edit_user.password = request.form.get("password")
+            edit_user.username = request.form.get("name")
+            edit_user.email = request.form.get("email")
+            edit_user.password = request.form.get("password")
 
-        db.session.add(edit_user)
-        db.session.commit()
+            db.session.add(edit_user)
+            db.session.commit()
 
-    flash("User edited successfully")
-    return redirect(url_for("routes.admin", db=dab))
-
-
-@routes.route("/delete")
-def delete():
-    dab = request.args.get("db")
-    id = request.args.get("id")
-
-    if (dab == "Books"):
-        delete_book = Books.query.get(id)
-        db.session.delete(delete_book)
-        db.session.commit()
-
-    elif (dab == "Users"):
-        delete_user = Users.query.get(id)
-        db.session.delete(delete_user)
-        db.session.commit()
-
-    return redirect(url_for("routes.admin", db = dab))
+        flash("User edited successfully")
+        return redirect(url_for("routes.admin", db=dab))
 
 
-@routes.route("/login", methods = ["GET", "POST"])
-def login():
-    if request.method == "POST":
-        name = request.form.get("name")
-        Email = request.form.get("email")
-        password = request.form.get("password")
+    @routes.route("/delete")
+    def delete():
+        dab = request.args.get("db")
+        id = request.args.get("id")
 
-        user = Users.query.filter(or_(username = name, email = Email)).first()
+        if (dab == "Books"):
+            delete_book = Books.query.get(id)
+            db.session.delete(delete_book)
+            db.session.commit()
 
-        if user:
-            if (user.password == password):
-                # login_user(user)
-                return redirect(url_for("dashboard"))
+        elif (dab == "Users"):
+            delete_user = Users.query.get(id)
+            db.session.delete(delete_user)
+            db.session.commit()
 
-        else:
-            flash("Invalid User Credentials")
+        return redirect(url_for("routes.admin", db = dab))
 
-    return render_template("login.html")
 
-@routes.route("/register", methods = ["GET", "POST"])
-def register():
+    @routes.route("/login", methods = ["GET", "POST"])
+    def login():
+        if request.method == "POST":
+            name = request.form.get("name")
+            password = request.form.get("password")
 
-    if (request.method == "POST"):
-        name = request.form.get("name")
-        email = request.form.get("email")
-        password = request.form.get("password")
+            user = Users.query.filter(or_(Users.username == name, Users.email == name)).first()
+            print(user)
 
-        user = Users(name, email, password)
-        db.session.add(user)
-        db.session.commit()
-        
-    return render_template("register.html")
+            if user:
+                if (user.password == password):
+                    login_user(user)
+                    return redirect(url_for("routes.dashboard"))
+
+            else:
+                flash("Invalid User Credentials")
+
+        return render_template("login.html")
+
+    @routes.route("/register", methods = ["GET", "POST"])
+    def register():
+
+        if (request.method == "POST"):
+            name = request.form.get("name")
+            email = request.form.get("email")
+            password = request.form.get("password")
+
+            user = Users(name, email, password)
+            db.session.add(user)
+            db.session.commit()
+            
+        return render_template("register.html")
 
 
 
-@routes.route("/dashboard")
-# @login_required
-def dashboard():
-    return "dashboard"
+    @routes.route("/dashboard")
+    @login_required
+    def dashboard():
+        return "dashboard"
+
+
+    return routes
